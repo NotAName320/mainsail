a3aaa48b4d64b3be40cc335a30b117afebb6d151
Ray Chen <rc144@outlook.com>
2023-07-22 21:23:30 -0700

    add license header

--- mainsail.py
+++ mainsail.py
@@ -1,3 +1,22 @@
+"""
+This file is part of mainsail, a regional infrastructure bot for
+NationStates (https://github.com/NotAName320/mainsail).
+Copyright (c) 2023 NotAName320.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import json
 import os
 import sys
